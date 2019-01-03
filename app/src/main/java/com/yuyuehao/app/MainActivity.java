1252caccddfb7be913e1d3c3490a0a8d76801df2
liwang <lwxyq2011@163.com>
2019-01-03 17:22:03 +0800

    v1.0  clientpool

--- app/src/main/java/com/yuyuehao/app/MainActivity.java
+++ app/src/main/java/com/yuyuehao/app/MainActivity.java
@@ -1,18 +1,26 @@
 package com.yuyuehao.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-import java.net.InetSocketAddress;
+import com.yuyuehao.andy.netty.NettyClientPool;
+import com.yuyuehao.andy.netty.NettyListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.nio.charset.Charset;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.channel.pool.SimpleChannelPool;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.FutureListener;
+
+public class MainActivity extends AppCompatActivity implements NettyListener{
 
 
-    //public InetSocketAddress addr1 = new InetSocketAddress("monitor.ailib.net.cn", 13000);
-    public InetSocketAddress addr2 = new InetSocketAddress("192.168.53.16", 8888);
+    private static final String TAG = "Main";
     private EditText editText;
 
 
@@ -21,39 +29,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = (EditText)findViewById(R.id.editText);
-        startService(new Intent(this,MyService.class));
+        //startService(new Intent(this,MyService.class));
+        NettyClientPool.getInstance().setListener(this);
+        try {
+            NettyClientPool.getInstance().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
     public void send80(View view){
-        final String ECHO_REQ = "Hello Netty.$_80";
-
+        final String ECHO_REQ = "Hello Netty.$_10000";
+        asyncWriteMessage("192.168.53.61:10000",ECHO_REQ);
 
 
     }
 
 
     public void send88(View view){
-        final String ECHO_REQ = "Hello Netty.$_88";
-
+        final String ECHO_REQ = "Hello Netty.$_10001";
+        asyncWriteMessage("192.168.53.61:10001",ECHO_REQ);
     }
 
     public void Send(View view){
-
         final String str = editText.getText().toString();
+        asyncWriteMessage("192.168.53.61:10002",str);
+    }
 
-        if (str.length() !=0){
-            if (str.startsWith("1")){
-                //final SimpleChannelPool pool = NettyClientPool.getInstance().poolMap.get(addr2);
-               //pool.close();
-            }else {
-                //asyncWriteMessage(addr1,"sdfasdfasdfa");
+    public void asyncWriteMessage(String  address, final String message) {
+        final SimpleChannelPool pool = NettyClientPool.getInstance().getPool(address);
+        Future<Channel> future = pool.acquire();
+        // 获取到实例后发消息
+        future.addListener(new FutureListener<Channel>() {
+            @Override
+            public void operationComplete(Future<Channel> channelFuture) throws Exception {
+                if (channelFuture.isSuccess()) {
+                    Channel ch = (Channel) channelFuture.getNow();
+                    StringBuffer sb = new StringBuffer();
+                    sb.append(message);
+                    sb.append("\n");
+                    ch.writeAndFlush(sb.toString());
+                    pool.release(ch);
+                }
             }
-        }
+        });
     }
 
-    public void asyncWriteMessage(final InetSocketAddress address, final String message) {
-
+    @Override
+    public void onMessageResponse(String inetSocketAddress, ByteBuf data) {
+        Log.i(TAG,inetSocketAddress+":"+data.toString(Charset.forName("utf-8")));
     }
 
+    @Override
+    public void onServiceStatusConnectChanged(String inetSocketAddress, int statusCode) {
+        Log.i(TAG,inetSocketAddress+":"+"statusCode = "+statusCode);
+    }
 }
